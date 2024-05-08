557575c26bd3ae26d9cbc40d73e44ad4927fac1e
parvina18 <parvinamamanazarova2@gmail.com>
2024-05-08 18:20:23 +0500

    history -> outcome - income

--- src/main/java/org/example/model/Transfer.java
+++ src/main/java/org/example/model/Transfer.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.*;
 
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 @Setter
